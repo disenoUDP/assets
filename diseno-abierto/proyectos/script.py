c2392ee9a894a94959dc8a78d8ac95d56b691afc
aarón montoya-moraga <montoyamoraga@gmail.com>
2023-12-08 12:29:08 -0300

    agregar JSON generado con codigo python

--- diseno-abierto/proyectos/script.py
+++ diseno-abierto/proyectos/script.py
@@ -1,14 +1,38 @@
-# importar modulo csv
+# importar modulos
 import csv
+import json
+
+# crear lista vacia
+lista = []
 
 # leer archivo .csv
-with open('test.csv', newline='') as csvfile:
+with open('test.csv', newline='', encoding='utf-8') as csvfile:
+  # crear objeto lector en formato diccionario
   reader = csv.DictReader(csvfile)
+
+  # por cada fila del archivo
   for row in reader:
-    print(row['Timestamp'])
-    print(row['Nombre del taller'])
-    print(row['Nombre del proyecto'])
-    print(row['Nombres completos'])
-    print(row['Texto explicativo'])
-    print(row['Etiquetas'])
-    print(row['Imagenes'])
+
+    # crear diccionario en blanco
+    diccionario = {}
+    # agregar items
+    diccionario['timestamp'] = row['Timestamp']
+    diccionario['sigla'] = row['Nombre del taller'].split(' ')[0]
+    diccionario['nombre'] = row['Nombre del proyecto']
+    diccionario['estudiantes'] = row['Nombres completos'].split(',')
+    diccionario['texto'] = row['Texto explicativo']
+    diccionario['etiquetas'] = row['Etiquetas'].split(',')
+    # diccionario['Imagenes'] = row['Imagenes'].split(',')
+    diccionario['url'] = 'https://placehold.co/400'
+    diccionario['thumbnailURL'] = 'https://placehold.co/400'
+
+    # agregar diccionario a lista
+    lista.append(diccionario)
+    
+
+# Serializing json
+json_object = json.dumps(lista, indent=2)
+ 
+# Writing to sample.json
+with open("proyectos.json", "w", encoding='utf-8') as outfile:
+    outfile.write(json_object)
